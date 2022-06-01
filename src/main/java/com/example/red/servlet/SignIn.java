2ac4c5e54866c330aaca9308c58fe27a0ffaf527
Emanuele <egalli64@gmail.com>
2022-06-01 14:37:39 +0200

    fix on access by servlet

--- src/main/java/com/example/red/servlet/SignIn.java
+++ src/main/java/com/example/red/servlet/SignIn.java
@@ -13,29 +13,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/start/signIn")
 public class SignIn extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SignIn() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String password = request.getParameter("password");
+		
+		String url;
+		if(password == null || password.isBlank()) {
+		    url = "/signin.html";
+		} else {
+		    url = "welcome.jsp";
+		}
+		
+        request.getRequestDispatcher(url).forward(request, response);
 	}
 
 }
