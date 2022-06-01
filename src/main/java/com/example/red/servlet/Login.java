4a15c1d0159e6ef1199fef8d8823ced9df5488c1
Giorgia <dalbagiorgia@gmail.com>
2022-06-01 17:34:10 +0200

    reservation form

--- src/main/java/com/example/red/servlet/Login.java
+++ src/main/java/com/example/red/servlet/Login.java
@@ -1,12 +1,13 @@
 package com.example.red.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 @WebServlet("/start/login")
 public class Login extends HttpServlet {
@@ -18,12 +19,13 @@ public class Login extends HttpServlet {
 		String password = request.getParameter("password");
 
 		String url;
-		if (username == null ||username.isBlank()|| password == null || password.isBlank()) {
+		if (username == null || username.isBlank() || password == null || password.isBlank()) {
 			url = "/login.html";
 		} else {
 			url = "welcome.jsp";
 		}
-
+		request.setAttribute("username", username);
 		request.getRequestDispatcher(url).forward(request, response);
+
 	}
 }
