4751cd7c5a5d3da5afa4a45d638fab841490cc05
Giorgia <dalbagiorgia@gmail.com>
2022-06-01 15:33:48 +0200

    new servlet login 2

---
+++ src/main/java/com/example/red/servlet/Login.java
@@ -0,0 +1,29 @@
+package com.example.red.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/start/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		String url;
+		if (username == null ||username.isBlank()|| password == null || password.isBlank()) {
+			url = "/login.html";
+		} else {
+			url = "welcome.jsp";
+		}
+
+		request.getRequestDispatcher(url).forward(request, response);
+	}
+}
