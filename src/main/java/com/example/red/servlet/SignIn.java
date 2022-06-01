03dbcacbf4e517e1a270fe061acac3d01d863da7
Giorgia <dalbagiorgia@gmail.com>
2022-06-01 15:01:43 +0200

    new parameters

--- src/main/java/com/example/red/servlet/SignIn.java
+++ src/main/java/com/example/red/servlet/SignIn.java
@@ -14,17 +14,25 @@ import javax.servlet.http.HttpServletResponse;
 public class SignIn extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String nome = request.getParameter("nome");
+		String cognome = request.getParameter("cognome");
+		String username = request.getParameter("username");
+		String email = request.getParameter("email");
+		String telefono = request.getParameter("telefono");
+
 		String password = request.getParameter("password");
-		
+
 		String url;
-		if(password == null || password.isBlank()) {
-		    url = "/signin.html";
+		if (nome == null
+				|| cognome == null || username == null || email==null && telefono ==null|| password == null || password.isBlank()) {
+			url = "/signin.html";
 		} else {
-		    url = "welcome.jsp";
+			url = "welcome.jsp";
 		}
 		
-        request.getRequestDispatcher(url).forward(request, response);
+		request.getRequestDispatcher(url).forward(request, response);
 	}
 
 }
